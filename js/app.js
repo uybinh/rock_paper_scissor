4fac725f71a650083f331edef48aa1cb70e868db
Uy Binh <uybinh@gmail.com>
2018-02-13 00:01:12 +0700

    Reduce repeatant to use DRY
    Use three function win() lose() draw() to avoid repeatant. DRY

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Let computer chose
 function computerPlay() {
   let choice = randomNum(3);
   switch (choice) {
@@ -10,56 +11,71 @@ function computerPlay() {
   }
 }
 
-// Return a random number from 1 to "num"
+// Return a random number from 1 to "num" to for computer to chose
 function randomNum(num) {
   return Math.floor(Math.random() * num) + 1;
 }
 
+// One single round of game
 function playRound(playerSelection, computerSelection) {
   playerSelection = prompt('What is your choice?').toLowerCase();
   computerSelection = computerPlay().toLowerCase();
   let point;
   switch (true) {
     case (playerSelection == 'rock' && computerSelection == 'paper'):
-      console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nYou Lose! Paper beats Rock.");
+      lose(playerSelection, computerSelection);
       point = -1;
       break;
     case (playerSelection == 'rock' && computerSelection == 'scissor'):
-      console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nYou Win! Rock beats Scissor.");
+      win(playerSelection, computerSelection);
       point = 1;
       break;
     case (playerSelection == 'paper' && computerSelection == 'rock'):
-      console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nYou Win! Paper beats Rock.");
+      win(playerSelection, computerSelection);
       point = 1;
       break;
     case (playerSelection == 'paper' && computerSelection == 'scissor'):
-      console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nYou Lose! Scissor beats Paper.");
+    lose(playerSelection, computerSelection);
       point = -1;
       break;
     case (playerSelection == 'scissor' && computerSelection == 'paper'):
-      console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nYou Win! Scissor beats Paper.");
+      win(playerSelection, computerSelection);
       point = 1;
       break;
     case (playerSelection == 'scissor' && computerSelection == 'rock'):
-      console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nYou Lose! Rock beats Scissor.");
+      lose(playerSelection, computerSelection);
       point = -1;
       break;
     default:
-      console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nIt is a DRAW.");
+      draw(playerSelection, computerSelection);
       point = 0;
   }
   return point;
 }
 
+// The main function to call the Game
 function game() {
-  let yourScore = 0;
+  let playerScore = 0;
   let compScore = 0;
   let score;
   for (let i = 1; i <= 3; i++) {
     score = playRound();
-    if (score === 1) yourScore += 1
+    if (score === 1) playerScore += 1
     else if (score == -1) compScore += 1;
   }
-  let result = (yourScore === compScore) ? 'DRAW!' : (yourScore > compScore) ? 'You Win!' : 'You Lose!';
-  return "score " + yourScore + " " + compScore + ". " + result;
+  let result = (playerScore === compScore) ? 'DRAW!' : (playerScore > compScore) ? 'You Win!' : 'You Lose!';
+  return "score " + playerScore + " " + compScore + ". " + result;
+}
+
+// Use three function lose() win() and draw() to reduce the text 'DRY'
+function lose(playerSelection, computerSelection) {
+  console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nYou Lose! " + computerSelection + " beats " + playerSelection + ".");
+}
+
+function win(playerSelection, computerSelection) {
+  console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nYou Win! " + playerSelection + " beats " + computerSelection + ".");
+}
+
+function draw(playerSelection, computerSelection) {
+  console.log("Your chose " + playerSelection + "\nComputer chose " + computerSelection + "\nIt is a DRAW.");
 }
